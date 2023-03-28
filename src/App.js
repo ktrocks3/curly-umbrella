a77be4d0447d41f9ba6ea2eba6918680b49b0009
Kishan <k.thakurani@sst.nl>
2023-03-28 15:08:24 +0200

    Testing webstorm

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
 function App() {
-  const value = 'Rennie';
-  return <div>Hello <span style={{color: "red"}}>{value}'s</span> site :D</div>;
+    const value = 'Rennie';
+    return <div>Hello <span style={{color: "red"}}>{value}'s</span> site :D</div>;
 }
 
 export default App;
